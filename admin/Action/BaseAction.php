b77340c9b79aac3e9395da5e53cdb255c732d643
jiaolongtian <haiyan.xu.vip@gmail.com>
2018-11-30 15:48:46 +0800

    *

---
+++ admin/Action/BaseAction.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * 基础action
+ * Date: 2018/11/30
+ * Time: 15:43
+ */
+namespace Admin\Action;
+
+use Frameworks\Tool\Http\HttpTool;
+use Illuminate\Http\Request;
+
+class BaseAction
+{
+    protected $request = null;
+    protected $httpTool = null;
+    protected $authService = null;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    protected function getHttpTool()
+    {
+        if (empty($this->httpTool)) {
+            $this->httpTool = new HttpTool($this->request);
+        }
+        return $this->httpTool;
+    }
+}
