ca891a2726fbce9c644fe342f6dd3be1db483b29
jiaolongtian <haiyan.xu.vip@gmail.com>
2018-12-05 17:40:55 +0800

    * 交易

---
+++ app/Http/Admin/Action/Order/Trade/IndexAction.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fallen
+ * Date: 2018/12/5
+ * Time: 17:05
+ */
+namespace App\Http\Admin\Action\Order\Trade;
+
+use Admin\Action\BaseAction;
+
+class IndexAction extends BaseAction
+{
+
+}
---
+++ app/Http/Admin/Controllers/Order/TradeController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 交易控制器
+ * Date: 2018/12/5
+ * Time: 17:03
+ */
+namespace App\Http\Admin\Controllers\Order;
+
+use App\Http\Admin\Controllers\Controller;
+
+class TradeController extends Controller
+{
+
+}
