9225f8c07c5644bc125271bf916cd89c9a35fecd
fallen <rogerk08@126.com>
2018-12-09 18:36:47 +0800

    *

---
+++ common/Models/User/UserApplyInfo.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 用户申请报名信息
+ * Date: 2018/12/9
+ * Time: 18:35
+ */
+namespace Common\Models\User;
+
+use Common\Models\BaseModel;
+
+class UserApplyInfo extends BaseModel
+{
+    protected $table = 'user_apply_infos';
+}
