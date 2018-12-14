b9ae40bdb75ab163f634b074e3e374649cfd6b14
jiaolongtian <haiyan.xu.vip@gmail.com>
2018-12-14 09:39:42 +0800

    * 新增入口

--- app/Http/Admin/Action/System/IndexAction.php
+++ app/Http/Admin/Action/System/IndexAction.php
@@ -7,12 +7,25 @@
 namespace App\Http\Admin\Action\System;
 
 use Admin\Action\BaseAction;
+use Admin\Config\AdminConfig;
 use Admin\Config\RouteConfig;
+use Admin\Services\Authority\Processor\AdminUserRoleProcessor;
 
 class IndexAction extends BaseAction
 {
     public function run()
     {
-        return redirect(route(RouteConfig::ROUTE_AUTHORITY_LIST));
+        $admin_info = $this->getAuthService()->getAdminInfo();
+        $roleId = $admin_info['role_id'];
+        $admin_url = route('admin.login');
+        if ($roleId > 0) {
+            $role = (new AdminUserRoleProcessor())->getSingleByNo($roleId);
+            $indexAction = $role->index_action;
+            if (!empty($indexAction)) {
+                $admin_url = AdminConfig::getIndexUrl($indexAction, 'url', 0);
+            }
+        }
+        header("location: {$admin_url}");
+        exit;
     }
 }
