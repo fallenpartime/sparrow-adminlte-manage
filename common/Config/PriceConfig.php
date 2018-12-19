7aaeca809156797f28fd3397746d7864e0579472
jiaolongtian <haiyan.xu.vip@gmail.com>
2018-12-19 18:06:38 +0800

    *

---
+++ common/Config/PriceConfig.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 报价配置
+ * Date: 2018/12/14
+ * Time: 0:59
+ */
+namespace Common\Config;
+
+class PriceConfig
+{
+    public static function getTypeList()
+    {
+        return [
+            '1'     =>  '报名',
+        ];
+    }
+}
